bf32c944b2e1f5c388fc8688a6e4f706cd669789
Javier <pardosblesajavier@gmail.com>
2025-12-26 21:26:24 +0100

    add github actions and modify astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+const LIVE_URL = "https://xlxxlxtxx.github.io";
+const SERVER_PORT = 4321;
+
 // https://astro.build/config
 export default defineConfig({
+    server: {
+        port: SERVER_PORT,
+    },
+    site: LIVE_URL,
     vite: {
         plugins: [tailwindcss()],
     },
